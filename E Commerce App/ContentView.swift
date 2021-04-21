051bbe154e781b1f051ac5d257e0686801ac1372
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-21 15:59:37 +0000

    Initial Commit

---
+++ E Commerce App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E Commerce App
+//
+//  Created by elhajjaji on 21/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ E Commerce App/E_Commerce_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_AppApp.swift
+//  E Commerce App
+//
+//  Created by elhajjaji on 21/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
